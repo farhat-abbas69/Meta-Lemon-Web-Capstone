5b0573e38cbfdc89567a5d8bbb37640bf5f46993
farhat-abbas69 <farhxt.abbxs69@gmail.com>
2023-02-23 17:38:08 +0500

    aesthetics

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -13,11 +13,12 @@ const HeroSection = () => {
                 <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
                 <NavLink to="/reservations"><button className="btn">Reserve a Table</button></NavLink>
             </div>
-            <div className="section-right">
-                <div className="image-box">
-                    <img src={HeroImage} alt="Serving delicious dish" />
+            {/* <div className="section-right">
+                
+            </div> */}
+            <div className="image-box">
+                    <img src="https://www.jigsawplanet.com/Jjwu2502/aesthetic-food?rc=face" alt="Serving delicious dish" />
                 </div>
-            </div>
         </div>
     </div>
   )
--- src/components/SpecialsCard.js
+++ src/components/SpecialsCard.js
@@ -1,29 +1,26 @@
 import React from 'react'
 import "../components/styles/SpecialsCard.css"
-import Dessert1 from "../components/assets/salad.jpg"
-import Dessert2 from "../components/assets/bruschetta1.jpg"
-import Dessert3 from "../components/assets/creme.jpg"
 import { MdDeliveryDining } from "react-icons/md"
 import { NavLink } from 'react-router-dom'
 
 const SpecialsCard = () => {
     const specialMenus = [
         {   
-            image: Dessert1,
-            title: "Greek Salad",
+            image: "https://cdn77-s3.lazycatkitchen.com/wp-content/uploads/2020/06/vegan-new-potato-salad-1000x1500.jpg",
+            title: "Supreme Salad",
             price: "$12.99",
             description: "The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.",
             order: "Order a delivery"
         },
         {
-            image: Dessert2,
-            title: "Bruschetta",
+            image: "https://www.peanutblossom.com/wp-content/uploads/2020/05/garlicbread-feature-1-720x720.jpg",
+            title: "Garlic Bread",
             price: "$5.99",
-            description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil; made perfect for an evening dinner.",
+            description: "Our Garlic Bread goes with every meal there is and is super tasty. It is affordable and something you would get addicted to.",
             order: "Order a delivery"
         },
         {
-            image: Dessert3,
+            image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d5c03a99139723.5eebce6281e84.jpeg",
             title: "Lemon Dessert",
             price: "$5.00",
             description: "This comes straight from Grandma's recipe book. Every last ingredient has been sourced and is as authentic as can be imagined.",
